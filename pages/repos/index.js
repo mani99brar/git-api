8cf90740bc7fb755e6bdad8930610a59648717cd
mani99brar <mani99brar@gmail.com>
2024-01-03 18:23:55 +0530

    feat:show repos

---
+++ pages/repos/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const Repositories = () => {
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        // Fetch the repositories when the component mounts
+        fetch('https://git-api-backend-production.up.railway.app/get-repos')
+            .then(response => response.json())
+            .then(data => setRepos(data))
+            .catch(error => console.error('Error fetching repos:', error));
+    }, []); // Empty dependency array means this effect runs once on mount
+
+    return (
+        <div>
+            <h1>User Repositories</h1>
+            <ul>
+                {repos.map(repo => (
+                    <li key={repo.id}>
+                        <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                            {repo.name}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Repositories;
